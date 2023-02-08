4fac1cff93ed2824aa21cf308e1366c5b6425a34
Rodstq <rodolfovanhouten@gmail.com>
2023-02-07 23:33:13 -0300

    adicionando mais questões

--- atividade 7/Avaliações/1periodoAV2.js
+++ atividade 7/Avaliações/1periodoAV2.js
@@ -32,22 +32,75 @@ QUESTÃO 2
 
 */
 
-function compara(array){
-    var maior = array[0];
-    for(var i =0; i<array.length;i++){
-        if(array[i] > maior){
-            maior = array[i];
-        }
-    }
-    var menor = array[0];
-    for(var i = 0; i< array.length;i++){
-        if(array[i] < menor){
-            menor = array[i];
-        }
-    }
-    var diferenca = maior - menor;
-    document.write(diferenca);
-}
-var m = [1,5,20,50,300];
+// function compara(array){
+//     var maior = array[0];
+//     for(var i =0; i<array.length;i++){
+//         if(array[i] > maior){
+//             maior = array[i];
+//         }
+//     }
+//     var menor = array[0];
+//     for(var i = 0; i< array.length;i++){
+//         if(array[i] < menor){
+//             menor = array[i];
+//         }
+//     }
+//     var diferenca = maior - menor;
+//     document.write(diferenca);
+// }
+// var m = [1,5,20,50,300];
 
-compara(m);
+// compara(m);
+
+/* 
+
+QUESTÃO 3
+
+*/
+
+// function retorna (a,b){
+//     for(;;){
+//         var n = parseInt(prompt("digite um número"));
+//         if( n >= a && n <= b){
+//             document.write("o numero digitado foi :" + n);
+//             break;
+//         }
+//     }
+// }
+
+// retorna(1,6);
+
+/*
+
+QUESTÃO 4
+
+*/
+
+function potencia(a,b){
+    var ind = a;
+    var c = a;
+    if(b == 0){
+        c = 1;
+        document.write(c);       
+    } else {
+        for(var i = 1; i < b ; i++){
+            c = c*ind;
+        }
+        document.write(`a base ${ind} multiplicada por indice ${b} é: ${c} <br>`);
+    }
+}
+
+function potenciaR(a,b){
+var c = 0;
+    if(b<=1){
+        return (b-1);
+    } else {
+        c = a * potenciaR(a-1);
+        document.write(c + "<br>");
+    }
+
+}
+
+potencia(10,2);
+potenciaR(10,2);
+
